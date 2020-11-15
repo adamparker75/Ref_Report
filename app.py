cf78df94133f6e7964e729ec8589b6a800f97477
adamparker75 <adamparker75@sky.com>
2020-11-15 18:06:34 +0000

    Created submit report page and app route in app.py file, uploaded image to display on the page

--- app.py
+++ app.py
@@ -91,6 +91,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/submit_report")
+def submit_report():
+    return render_template("/submit_report.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
